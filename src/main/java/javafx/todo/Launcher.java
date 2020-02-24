5b645b6239fe2c54664bda5e6009bca0427bfc8b
Kevin Putera <kevintiosanna@gmail.com>
2020-02-24 17:32:44 +0800

    Bootstrap javafx application

---
+++ src/main/java/javafx/todo/Launcher.java
@@ -0,0 +1,9 @@
+package javafx.todo;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
